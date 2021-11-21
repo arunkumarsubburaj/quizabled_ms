deb6f963f9042e7ef14d618edad36308a8deb9d7
arunkumarsubburaj <arun@appsmav.com>
2021-11-21 13:08:10 +0530

    Added Quiz Controller

---
+++ controllers/quiz.js
@@ -0,0 +1,89 @@
+var sql = require("mssql");
+var getQuestions = async function (req, res) {
+  const getQuery = `select * from quiz_questions where 
+  quiz_questions.isActive = 1 
+  and 
+  quiz_questions.languageCode='en' 
+  and
+  quiz_questions.category='${req.body.category}'`;
+  try {
+    const result = await sql.query(getQuery);
+    let questionIds = "";
+    result.recordset.forEach((questionObj, index, resultArray) => {
+      questionIds += questionObj.questionId;
+      if (resultArray.length - 1 != index) {
+        questionIds += ",";
+      }
+    });
+    const primaryQuestions = result.recordset;
+    const regionalQuestions = await getRegionalQuestions(req, res, questionIds);
+    let regionalQuestionIds = "";
+    regionalQuestions.forEach((questionObj, index, resultArray) => {
+      regionalQuestionIds += questionObj.questionId;
+      if (resultArray.length - 1 != index) {
+        regionalQuestionIds += ",";
+      }
+    });
+
+    const primaryOptions = await getPrimaryOptions(req, res, questionIds);
+    const regionalOptions = await getRegionalOptions(
+      req,
+      res,
+      regionalQuestionIds
+    );
+    const questionsSet = {
+      primaryQuestionsObj: {
+        questions: primaryQuestions,
+        options: primaryOptions,
+      },
+      secondaryQuestionObj: {
+        questions: regionalQuestions,
+        options: regionalOptions,
+      },
+    };
+    res.status(200).send(questionsSet);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+var getRegionalQuestions = async function (req, res, primaryQuestionIds) {
+  const getSecondaryQuestionsQuery = `select * from quiz_questions where 
+    quiz_questions.isActive = 1 
+    and 
+    quiz_questions.languageCode='${req.body.language}' 
+    and 
+    quiz_questions.primaryQuestionId in (${primaryQuestionIds})`;
+  try {
+    const result = await sql.query(getSecondaryQuestionsQuery);
+    return result.recordset;
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+var getPrimaryOptions = async function (req, res, primaryQuestionIds) {
+  const getOptionsQuery = `select * from quiz_options where 
+  quiz_options.isActive = 1 
+  and 
+  quiz_options.questionId in (${primaryQuestionIds})`;
+  try {
+    const result = await sql.query(getOptionsQuery);
+    return result.recordset;
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+var getRegionalOptions = async function (req, res, reqionalQuestionIds) {
+  const getOptionsQuery = `select * from quiz_options where 
+    quiz_options.isActive = 1 
+    and 
+    quiz_options.questionId in (${reqionalQuestionIds})`;
+  try {
+    const result = await sql.query(getOptionsQuery);
+    return result.recordset;
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+exports.QuizController = {
+  getQuestions: getQuestions,
+};
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { UserController } = require("./controllers/user");
 const { LoginController } = require("./controllers/login");
 const { ImageUploadController } = require("./controllers/imageUpload");
 const { QuestionController } = require("./controllers/questions");
+const { QuizController } = require("./controllers/quiz");
 var app = express();
 app.use(cors());
 app.use(express.json());
@@ -44,6 +45,7 @@ app.post(
   ImageUploadController.imageUpload
 );
 app.post("/addQuestions", QuestionController.addQuestions);
+app.get("/getQuestions", QuizController.getQuestions);
 
 var server = app.listen(5000, function () {
   console.log("Server is running..");
